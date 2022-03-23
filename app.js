0ec2b67158bd9cfe4586f2500390f0f8273745fb
tallguy188 <cksghd4220@naver.com>
2022-03-23 17:52:25 +0900

    Add tag Event with JS

--- app.js
+++ app.js
@@ -1,28 +1,64 @@
 
 const body = document.querySelector("body");
-const form  = document.querySelector("form");
+const form = document.querySelector("form");
 const input = document.querySelector("input");
+const taglist = document.querySelector(".tagList");
+const s = document.querySelector("span");
 
+
+
+let toDos = [];
 
 function handleSubmit(event) {
     event.preventDefault();
     const textvalue = input.value;
     loadImage(textvalue);
-    input.value ="";
+    handleTag(textvalue);
+    input.value = "";    
+}
+
+function handleClick(event){
+    console.log(event);
+    const inputvalue = event.target.innerText;
+    loadImage(inputvalue);
+}
+
+function handleTag(word) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const newId = toDos.length + 1;
+    span.innerText = word;
+    li.appendChild(span);
+    li.id = newId;
+    taglist.appendChild(li);
+    toDos.push(1);
+    setRandomColor(span);
+    if(span) {
+        span.addEventListener("click",handleClick);
+    }
+}
+
+
+function setRandomColor(p) {
+    var x = Math.floor(Math.random() * 256);
+    var y =  Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgcolor = "rgba(" + x + "," + y + ","+ z + "," + 0.3 +")";
+    p.style.backgroundColor = bgcolor;
+    
+    
 
 }
 
 function loadImage(keyword) {
-   fetch(`https://source.unsplash.com/featured/?${keyword}`)
-   .then(function(response) {
-    body.style.backgroundImage = `url(${response.url})`;
-   });
-  
+    fetch(`https://source.unsplash.com/featured/?${keyword}`)
+        .then(function (response) {
+            body.style.backgroundImage = `url(${response.url})`;
+        });
 }
 
 
-if(form) {
-    form.addEventListener("submit",handleSubmit);
-    
-}
 
+if (form) {
+    form.addEventListener("submit", handleSubmit);
+}
